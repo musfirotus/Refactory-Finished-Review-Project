6c2a8ec654ec157eab4c3e9d1f63560754247ef1
Musfirotus <musfirotus@gmail.com>
2020-08-01 20:26:59 +0700

    add & update todo list

--- Week 2/03 Todo-CLI-App/index.js
+++ Week 2/03 Todo-CLI-App/index.js
@@ -8,9 +8,9 @@ program.version("1.0.0").description("ToDoList-CLI-APP");
 const listTodo = async () => {
     const todoList = await Todo.findAll({raw:true});
     console.log("Todo List :");
-    Object.values(todoList).forEach(lis => {
+    Object.values(todoList).forEach((lis) => {
         if(lis.cek == 'Done') console.log(`${lis.id}. ${lis.item}. (${lis.cek})`);
-        else if(lis.cek == 'Undone') console.log(`${lis.id}. ${lis.item}.`);
+        else if(lis.cek == 'Undone' || lis.cek == '') console.log(`${lis.id}. ${lis.item}.`);
         else console.log('Error!');
     })
     await sequelize.close()
@@ -23,9 +23,34 @@ const error = (error, result)=>{
 }
 
 program
-.command("todo list","show list")
-.action(async () => {
-    listTodo();
-})
+    // Show all todo list
+    // How to run : node index.js todo list
+    .command("todo list","show list")
+    .action(async () => {
+        await listTodo();
+    })
+
+    // Tambah data todo list
+    // How to run : node index.js todo add <item>
+    .command("todo add", "Tambah todo list")
+    .argument("<item>", "list yang ingin ditambahkan")
+    .action(async ({ args }) => {
+        await Todo.create({ item: args.item, cek:"" });
+        await listTodo();
+    })
+
+    // Edit data todo list
+    // How to run : node index.js todo update <id> <item>
+    .command("todo update", "Edit todo list")
+    .argument("<id>", "Id yang ingin diubah")
+    .argument("<item>", "Todo list baru")
+    .action(async ({ args }) => {
+        await Todo.update({ item: args.item }, {
+            where: {
+                id: args.id
+            }
+        });
+        await listTodo();
+    })
 
 program.run()
