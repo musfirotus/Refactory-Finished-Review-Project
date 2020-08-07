4719fa688259a1d3ba904a783ff58b57cd64f7f9
Musfirotus <musfirotus@gmail.com>
2020-08-07 16:18:17 +0700

    3

--- Week 1/02 JS-Challenge/003.js
+++ Week 1/02 JS-Challenge/003.js
@@ -1,3 +1,4 @@
+
 const csv =`NAME, CATEGORY, PRICE
 Xiaomi Redmi 5A, Smartphone, 1199000
 Macbook Air, Laptop, 13775000
@@ -10,44 +11,38 @@ const { promises } = require('fs')
 
 const [header, ...array] = csv.split("\n")
 
-const obj = array.map((val) => {
-    const items = val.split(", ")
-    const obj = {}
+const obj = array.map((value) => {
+  const items = value.split(", ")
+  const obj = {}
 
-    header
-        .split(", ")
-        .map((val) => val.toLowerCase())
-        .forEach((value, index) => {
-            if (value == "price") {
-                obj[value] = Intl.NumberFormat("id", { style: 'currency', currency: "IDR" })
-                                    .format(parseInt(items[index]))
-            } else {
-                obj[value] = items[index]
-            }
-        })
-    
-    return obj
+  header.split(", ").map((value) => value.toLowerCase()).forEach((value, index) => {
+    if (value == "PRICE") {
+      obj[value] = new Intl.NumberFormat("id", { style: 'currency', currency: "IDR" }).format(parseInt(items[index]))
+    } else {
+      obj[value] = items[index]
+    }
+  })
+
+  return obj;
 })
 
 promises.readFile('003.csv').then((value) => {
-    const [header, ...array] = value.toString('utf8').split("\n")
+  const [header, ...array] = value.toString('utf8').split("\n")
 
-    const obj = array.map((value) => {
-        const items = value.split(", ")
-        const obj = {}
-
-        header
-            .split(", ")
-            .map((val) => val.toLowerCase())
-            .forEach((value, index) => {
-                if (value == "price") {
-                    obj[value] = Intl.NumberFormat("id", { style: 'currency', currency: "IDR" })
-                                        .format(parseInt(items[index]))
-                } else {
-                    obj[value] = items[index]
-                }
-            })
-        
-        return obj
+  const obj = array.map((value) => {
+    const items = value.split(", ")
+    const obj = {}
+  
+    header.split(", ").map((value) => value.toLowerCase()).forEach((value, index) => {
+      if (value == "PRICE") {
+        obj[value] = new Intl.NumberFormat("id", { style: 'currency', currency: "IDR" }).format(parseInt(items[index]))
+      } else {
+        obj[value] = items[index]
+      }
     })
+  
+    return obj;
+  })
+  
+  console.log(obj)
 })
